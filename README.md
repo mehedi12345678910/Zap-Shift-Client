bcd1e86748105c28c27cdba9e4f1f193f64a964e
md mehedi hassan <mdmehedi01326018868@gmail.com>
2025-12-04 23:19:20 +0600

    added part 6

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # use  client side
-**react icon , react-router,dasyUI,Tailwind,responsive banner, npm install swiper,npm install react-leaflet@next,,npm install react-hook-form,npm install axios,npm install sweetalert2,npm i @tanstack/react-query
+**react icon , react-router,dasyUI,Tailwind,responsive banner, npm install swiper,npm install react-leaflet@next,,npm install react-hook-form,npm install axios,npm install sweetalert2,npm i @tanstack/react-query,npm install lottie-react
+
 # uses  server side
 **npm i express cors dotenv mongodb,npm install --save stripe,,stripe
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,3 +1,107 @@
+// import { createBrowserRouter } from "react-router";
+// import RootLayout from "../layouts/RootLayout";
+// import Home from "../pages/Home/Home/Home";
+// import Coverage from "../pages/Coverage/Coverage";
+// import AuthLayout from "../layouts/AuthLayout";
+// import Login from "../pages/Auth/Login/Login";
+// import Register from "../pages/Auth/Register/Register";
+// import PrivateRoute from "./PrivateRoute";
+// import Rider from "../pages/Rider/Rider";
+// import SendParcel from "../pages/sendParcel/SendParcel";
+// import DashboardLayout from "../layouts/DashboardLayout";
+// import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
+// import Payment from "../pages/Dashboard/Payment/Payment";
+// import PaymentSuccess from "../pages/Dashboard/Payment/paymentSuccess";
+// import PaymentCancelled from "../pages/Dashboard/Payment/PaymentCancelled";
+// import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
+// import ApproveRiders from "../pages/Dashboard/ApproveRiders/ApproveRiders";
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     Component: RootLayout,
+//     children: [
+//       {
+//         index: true,
+//         Component: Home,
+//       },
+//       {
+//         path: "rider",
+//         element: (
+//           <PrivateRoute>
+//             <Rider></Rider>
+//           </PrivateRoute>
+//         ),
+//         loader: () => fetch("/serviceCenters.json").then((res) => res.json()),
+//       },
+//       {
+//         path: "send-parcel",
+//         element: (
+//           <PrivateRoute>
+//             <SendParcel></SendParcel>
+//           </PrivateRoute>
+//         ),
+//         loader: () => fetch("/serviceCenters.json").then((res) => res.json()),
+//       },
+//       {
+//         path: "coverage",
+//         Component: Coverage,
+//         loader: () => fetch("/serviceCenters.json").then((res) => res.json()),
+//       },
+//     ],
+//   },
+//   {
+//     path: "/",
+//     Component: AuthLayout,
+//     children: [
+//       {
+//         path: "login",
+//         Component: Login,
+//       },
+//       {
+//         path: "register",
+//         Component: Register,
+//       },
+//     ],
+//   },
+//   {
+//     path: "dashboard",
+//     element: (
+//       <PrivateRoute>
+//         <DashboardLayout></DashboardLayout>
+//       </PrivateRoute>
+//     ),
+//     children: [
+//       {
+//         path: "my-parcels",
+//         Component: MyParcels,
+//       },
+//       {
+//         path: "payment/:parcelId",
+//         Component: Payment,
+//       },
+//       {
+//         path: "payment-history",
+//         Component: PaymentHistory,
+//       },
+//       {
+//         path: "payment-success",
+//         Component: PaymentSuccess,
+//       },
+//       {
+//         path: "payment-cancelled",
+//         Component: PaymentCancelled,
+//       },
+//       {
+//         path: "approve-riders",
+//         Component: ApproveRiders,
+//       },
+//     ],
+//   },
+// ]);
+
+// part 6
+
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
@@ -11,10 +115,13 @@ import SendParcel from "../pages/sendParcel/SendParcel";
 import DashboardLayout from "../layouts/DashboardLayout";
 import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
 import Payment from "../pages/Dashboard/Payment/Payment";
-import PaymentSuccess from "../pages/Dashboard/Payment/paymentSuccess";
+import PaymentSuccess from "../pages/Dashboard/Payment/PaymentSuccess";
 import PaymentCancelled from "../pages/Dashboard/Payment/PaymentCancelled";
 import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
 import ApproveRiders from "../pages/Dashboard/ApproveRiders/ApproveRiders";
+import UsersManagement from "../pages/Dashboard/UsersManagement/UsersManagement";
+import AdminRoute from "./AdminRoute";
+import AssignRiders from "../pages/Dashboard/AssignRiders/AssignRiders";
 
 export const router = createBrowserRouter([
   {
@@ -94,7 +201,28 @@ export const router = createBrowserRouter([
       },
       {
         path: "approve-riders",
-        Component: ApproveRiders,
+        element: (
+          <AdminRoute>
+            <ApproveRiders></ApproveRiders>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "assign-riders",
+        element: (
+          <AdminRoute>
+            <AssignRiders></AssignRiders>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "users-management",
+        // Component: UsersManagement,
+        element: (
+          <AdminRoute>
+            <UsersManagement></UsersManagement>
+          </AdminRoute>
+        ),
       },
     ],
   },
